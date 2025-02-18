dde45976bb68c246b38f52689dbc86442c123e5c
jhoannymsg <jhoanny.mariesg@gmail.com>
2025-02-18 14:29:06 -0400

    Salchipapa

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿public class Persona
+{
+    public string Nombre { get; set; }
+    public string CorreoElectronico { get; set; }
+
+    public void EnviarCorreo(string mensaje)
+    {
+        Console.WriteLine($"Enviando correo a {CorreoElectronico}: {mensaje}");
+    }
+}
+public class EnviarCorreo
+{
+    public string Correo(string CorreoElectrinico, string mensaje)
+    {
+        Console.WriteLine($"Se envio un correo electronico a {CorreoElectrinico}, con el mensaje: {mensaje}")
+    }
+}
